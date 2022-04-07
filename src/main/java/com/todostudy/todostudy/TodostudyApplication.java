31a91d9bfb73e7eb488c2f1921aa4fb626626a0d
rlaalstjd00 <timkim01@naver.com>
2022-04-07 09:48:20 +0900

    test

--- src/main/java/com/todostudy/todostudy/TodostudyApplication.java
+++ src/main/java/com/todostudy/todostudy/TodostudyApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodostudyApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TodostudyApplication.class, args);
+		// todoList Test
 	}
 
 }
