76516be41eb6739b8fa6f258e75020bbda868dad
rlaalstjd00 <timkim01@naver.com>
2022-04-07 09:44:04 +0900

    todo_project init

---
+++ src/main/java/com/todostudy/todostudy/TodostudyApplication.java
@@ -0,0 +1,13 @@
+package com.todostudy.todostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodostudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodostudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todostudy/todostudy/TodostudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todostudy.todostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodostudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
